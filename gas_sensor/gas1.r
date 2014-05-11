ffb6dd585433696f6b91e49d5677c0b65ef9840a
Puneet Bansal <danny.explorer@gmail.com>
2014-05-11 08:57:09 -0500

    Writing batch id

--- gas_sensor/gas1.r
+++ gas_sensor/gas1.r
@@ -1,5 +1,5 @@
 #Import preprocessed gas sensor array drift data set using space as the separator
-gasdata = read.table(paste(Sys.getenv("HOME"),"/mr/preprocess/batch1.dat",sep=""),header=F);
+gasdata = read.table(paste(Sys.getenv("HOME"),"/mr/preprocess/allbatches.dat",sep=""),header=F);
 
 #Select only ethanol
 eth = gasdata[which(gasdata$V1 == 1),];
