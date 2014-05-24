1612d9924f9f077671746f18aeb2234bef0046bb
Puneet Bansal <danny.explorer@gmail.com>
2014-05-24 15:27:34 -0500

    Added decision tree example.

---
+++ DecisionTrees/biopsy.r
@@ -0,0 +1,17 @@
+library("MASS");
+data("biopsy");
+biopsy = biopsy[,-1];       
+biopsy = na.omit(biopsy);   
+
+#Gini split
+biopsy.fit.gini <- rpart(class ~ V1+V2+V3+V4+V5+V6+V7+V8+V9, data=biopsy, method="class",parms=list(split="gini"),maxdepth=2)
+plot(biopsy.fit.gini);
+text(biopsy.fit.gini);
+print(biopsy.fit.gini);
+
+#Entropy split
+biopsy.fit.entropy <- rpart(class ~ V1+V2+V3+V4+V5+V6+V7+V8+V9, data=biopsy, method="class",parms=list(split="information"),maxdepth=2);
+plot(biopsy.fit.entropy);
+text(biopsy.fit.entropy);
+print(biopsy.fit.gini);
+
